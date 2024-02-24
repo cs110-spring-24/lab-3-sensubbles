ce3a5eb4bceff9d27df546c2664fde675ec970e5
rooneybewildin <rooneyrojas2004@gmail.com>
2024-02-23 21:52:21 -0800

    new changes

--- for_loops.py
+++ for_loops.py
@@ -38,24 +38,22 @@ if run == 3:
 
 if run == 4:
     for count in range(0, 1001,2):
-        if count % 3 == 0:
-            print(count)
-        elif count % 5 == 0:
+        if count % 3 == 0 or count % 5 == 0:
             print(count)
 
 #5. Write a program that asks the user to enter a number and prints out all the numbers between 1 and that number that are divisible by 11 or 13. The number entered should be greater than 200. (+10 points) Extra credit if the programs asks again if the number is less than 200. (+5 points)
 
 if run == 5:
-    user = input("Enter a number equal to or greater than 200:")
-    user = int(user)
-    if user >= 200:
-        for count in range(0,user):
-            if count % 11 == 0:
-                print(count)
-            elif count % 13 == 0:
-                print(count)
-    elif user < 200:
-        input("Number must be equal to or greater than 200. Please enter a valid number: ")
+    while True:
+        user = input("Enter a number equal to or greater than 200:")
+        user = int(user)
+        if user >= 200:
+            for count in range(0, user):
+                if count % 11 == 0 or count % 13 == 0:
+                    print(count)
+            break
+        else:
+            print("Number must be equal to or greater than 200. Please enter a valid number.")
 
 #6. Write a program that prints out each letter of a string line by line (+5 points)
 
@@ -67,12 +65,14 @@ if run == 6:
 #7. Write a program that asks the user to enter a string and outputs every second letter. The string must be more than 10 letters long. (+10 points)
 
 if run == 7:
-    string = input("Enter a string of text more than 10 letters long: ")
-    if len(string) >= 10:
-        for l in range(2, len(string), 2):
-            print(string[l])
-    if len(string) < 10:
-        input("The string of text you input is less than 10 characters. Please enter a string of text:")
+    while True:
+        string = input("Enter a string of text more than 10 letters long: ")
+        if len(string) >= 10:
+            for l in range(2, len(string), 2):
+                print(string[l])
+                break
+        if len(string) < 10:
+            input("The string of text you input is less than 10 characters. Please enter a string of text:")
 #8. Write a program that rolls 1000 dice and prints out the number of times each number was rolled. (+15 points)
 
 ones = 0
@@ -108,18 +108,26 @@ if run == 8:
 #9. Write a program that checks if a given number is a prime number. A prime number is a number that is only divisible by 1 and itself. The user enters a number and the programs prints out whether the number is a prime number or not. (+15 points)
 
 if run == 9:
-    user = input("Enter a number to check if the number given is a prime number: ")
-    user = int(user)
-    if user / 1 == user:
-        if user / user == 1:
-            print(f"the number {user} is indeed is a prime number!")
-        else:
-            print(f"the number {user} is not a prime number...")
+    user = int(input("Enter a number to check if it's a prime number: "))
+    prime = True
+    for i in range(2, int(user**0.5) + 1):
+        if user % i == 0:
+            prime = False
+            break
+    if user > 1 and prime:
+        print(f"The number {user} is indeed a prime number!")
+    else:
+        print(f"The number {user} is not a prime number...")
 
 #10. Write a program that prints out the prime numbers less than 1000. (+20 points)
 
 if run == 10:
-   for primes in range(1,1001):
-       if primes / 1 == primes:
-           if primes / primes == 1:
-               print(primes)
+    for num in range(2, 1001):
+        prime = True
+        for primes in range(2, int(num**0.5) + 1):
+            if num % primes == 0:
+                prime = False
+                break
+
+        if prime:
+            print(num)
